f2f59952b79be908a1c0801a168214ab59d9cc5e
Matias Etcheverry <matias.etcheverry9@gmail.com>
2022-03-07 12:32:17 +0100

    delete code on import

--- custom_models/models.py
+++ custom_models/models.py
@@ -171,29 +171,3 @@ class PreTrainedResnet(BaseModel):
         # x = x.view(x.size(0), -1)
         # x = self.linear_layers(x)
         return x
-
-
-config = {}
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
-config["job_type"] = run.job_type if "run" in locals() else "test"
-config["train_val_split"] = 0.7
-config["seed"] = 42
-config["batch_size"] = 64
-config["learning_rate"] = 0.00001
-config["device"] = device
-config["momentum"] = 0.9
-config["architecture"] = "pretrained VGG"
-config["input_width"] = 64
-config["weight_decay"] = 0.00005
-config["epochs"] = 0
-config["frac_sample"] = 1
-config["frac_noise"] = 0.1
-config["nb_image_per_axis"] = 1
-config["total_layers"] = 1000
-config["fixed_layers"] = 0
-config["log_wandb"] = False
-model = PreTrainedResnet(config)
-total_params = sum(p.numel() for p in model.parameters())
-print(f"[INFO]: {total_params:,} total parameters.")
-print(model(torch.rand((1, 3, config["input_width"], config["input_width"]))).shape)
